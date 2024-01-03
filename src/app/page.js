9ebb706098a212e6977dba8049d7a5b550725695
Michael S.O.S Johnson <113233530+SOSJ001@users.noreply.github.com>
2024-01-03 15:49:24 +0000

    chartbot included

--- src/app/page.js
+++ src/app/page.js
@@ -3,9 +3,18 @@ export default function Home() {
   return (
     <main className="flex flex-col items-center p-24 space-y-4">
       <h1 className=" text-2xl">TESTING THE CHARTBOT</h1>
-      <div className=" p-5 border-3 border rounded-lg border-red-400 w-full h-screen flex justify-center ">
-        THIS IS WHERE YOUR CODE WILL GO
+      <div className="space-y-5 overflow-hidden p-5 border-3 border rounded-lg border-red-400 w-full flex-row items-center justify-center ">
+        <div className="w-full text-center">THIS IS WHERE YOUR CODE WILL GO</div> 
+        <div className="w-full text-center">The chartbot is down by the buttom right</div>
+        
       </div>
+      <script src="https://www.gstatic.com/dialogflow-console/fast/messenger/bootstrap.js?v=1"></script>
+<df-messenger
+  intent="WELCOME"
+  chat-title="UTS_CHATBOT"
+  agent-id="7fb54c25-55c9-4a10-9a95-e9cc608cbc15"
+  language-code="en"
+></df-messenger>
     </main>
   )
 }
