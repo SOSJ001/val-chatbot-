2a2d4835c8d97b8d126f08a962e18f875f626ffc
Michael S.O.S Johnson <113233530+SOSJ001@users.noreply.github.com>
2024-01-03 16:45:03 +0000

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,25 +1,29 @@
-import { Inter } from 'next/font/google'
-import './globals.css'
+import { Inter } from 'next/font/google';
+import Head from 'next/head';
+import './globals.css';
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ['latin'] });
 
 export const metadata = {
   title: 'chartbot',
-  description: 'Val`s chartbot',
-}
+  description: 'Val chartbot',
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}
-      <script src="https://www.gstatic.com/dialogflow-console/fast/messenger/bootstrap.js?v=1"></script>
-<df-messenger
-  intent="WELCOME"
-  chat-title="UTS_CHATBOT"
-  agent-id="7fb54c25-55c9-4a10-9a95-e9cc608cbc15"
-  language-code="en"
-></df-messenger>
+      <Head>
+        <script src="https://www.gstatic.com/dialogflow-console/fast/messenger/bootstrap.js?v=1"></script>
+      </Head>
+      <body className={inter.className}>
+        {children}
+        <df-messenger
+          intent="WELCOME"
+          chat-title="UTS_CHATBOT"
+          agent-id="7fb54c25-55c9-4a10-9a95-e9cc608cbc15"
+          language-code="en"
+        ></df-messenger>
       </body>
     </html>
-  )
+  );
 }
